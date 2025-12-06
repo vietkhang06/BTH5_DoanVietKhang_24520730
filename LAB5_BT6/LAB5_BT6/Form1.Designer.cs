a5e61de919ccaa5276e7e549c053d1aae0fc5ca4
vietkhang <109060462+vietkhang06@users.noreply.github.com>
2025-12-06 15:18:47 +0700

    Add files via upload

---
+++ LAB5_BT6/LAB5_BT6/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace LAB5_BT6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.lbFonts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbFonts
+            // 
+            this.lbFonts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbFonts.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.lbFonts.FormattingEnabled = true;
+            this.lbFonts.HorizontalScrollbar = true;
+            this.lbFonts.ItemHeight = 30;
+            this.lbFonts.Location = new System.Drawing.Point(35, 27);
+            this.lbFonts.Name = "lbFonts";
+            this.lbFonts.Size = new System.Drawing.Size(461, 424);
+            this.lbFonts.TabIndex = 0;
+            this.lbFonts.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lbFonts_DrawItem);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 450);
+            this.Controls.Add(this.lbFonts);
+            this.Name = "Form1";
+            this.Text = "All Fonts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ListBox lbFonts;
+    }
+}
+
---
+++ LAB5_BT6/LAB5_BT6/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB5_BT6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection installedFonts = new InstalledFontCollection();
+            foreach (FontFamily font in installedFonts.Families)
+            {
+                lbFonts.Items.Add(font.Name);
+            }
+        }
+
+        private void lbFonts_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+            e.DrawBackground();
+            string fontName = lbFonts.Items[e.Index].ToString();
+            Font fontToDraw;
+            try
+            {
+                fontToDraw = new Font(fontName, 14, FontStyle.Regular);
+            }
+            catch
+            {
+                fontToDraw = new Font("Arial", 14, FontStyle.Regular);
+            }
+            Brush textBrush;
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+            {
+                textBrush = SystemBrushes.HighlightText; 
+            }
+            else
+            {
+                textBrush = SystemBrushes.WindowText; 
+            }
+            e.Graphics.DrawString(fontName, fontToDraw, textBrush, e.Bounds.X, e.Bounds.Y + 2);
+            fontToDraw.Dispose();
+            e.DrawFocusRectangle();
+        }
+    }
+}
